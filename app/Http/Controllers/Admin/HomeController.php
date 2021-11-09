d4e3e5f78b1e151ad4450cf954fba867240755b4
Your Name <you@example.com>
2021-11-09 20:50:55 +0200

    first commit

---
+++ app/Http/Controllers/Admin/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\models\post;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $posts_count = Post::all()->count();
+
+        return view('admin.home.index', [
+            'posts_count' => $posts_count
+        ]);
+    }
+}
---
+++ public/js/script.js
@@ -0,0 +1,46 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!********************************!*\
+  !*** ./resources/js/script.js ***!
+  \********************************/
+$(document).ready(function () {
+  $('.header_burger').click(function (event) {
+    $('.header_burger,.header_menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+});
+document.getElementById('slider-left').onclick = sliderLeft;
+var left = 0;
+
+function sliderLeft() {
+  var polosa = document.getElementById('polosa');
+  var width = window.outerWidth;
+  left = left - 80;
+
+  if (width < 820) {
+    if (left < -160) {
+      left = 0;
+    }
+  } else {
+    if (left < -80) {
+      left = 0;
+    }
+  }
+
+  polosa.style.left = left + 'px';
+}
+
+document.getElementById('slider-right').onclick = sliderRight;
+
+function sliderRight() {
+  var polosa = document.getElementById('polosa');
+  left = left + 80;
+
+  if (left > 0) {
+    left = 0;
+  }
+
+  polosa.style.left = left + 'px';
+}
+/******/ })()
+;
